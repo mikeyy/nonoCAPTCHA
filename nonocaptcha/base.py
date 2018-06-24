678003a9450d69b08541d8f3b4b4a5cccb4bddb7
Michael Mooney <mikeyy@mikeyy.com>
2018-06-24 04:24:42 -0500

    detection issues resolved

--- nonocaptcha/base.py
+++ nonocaptcha/base.py
@@ -23,9 +23,9 @@ class Base(Clicker):
     logger = logging.getLogger(__name__)
     if settings["debug"]:
         logger.setLevel("DEBUG")
-
+    
     detected = False
-
+    
     def get_frames(self):
         self.checkbox_frame = next(
             frame for frame in self.page.frames if "api2/anchor" in frame.url
@@ -38,11 +38,11 @@ class Base(Clicker):
     async def click_reload_button(self):
         reload_button = await self.image_frame.J("#recaptcha-reload-button")
         await self.click_button(reload_button)
-
+        
     async def check_detection(self, frame, timeout, wants_true=""):
         """Checks if "Try again later", "please solve more" modal appears 
         or success"""
-
+    
         if wants_true:
             wants_true = f"if({wants_true}) return true;"
     
@@ -50,35 +50,34 @@ class Base(Clicker):
         #    l = [f'if({i}) return true;' for i in wants_true]
         #    wants_true = '\n'.join(wants_true)
     
-        func ="""() => {
-            %s
-            
-            var elem_bot = parent.frames[1].document.getElementsByClassName
-                ("rc-doscaptcha-header-text")[0];
-            if(typeof elem_bot !== 'undefined'){
-                if(elem_bot.innerText === 'Try again later'){
-                    parent.window.wasdetected = true;
-                    return true;
-                }
-            }
-    
-            var elem_try = parent.frames[1].document.getElementsByClassName
-                ("rc-audiochallenge-error-message")[0];
-            if(typeof elem_try !== 'undefined'){
-                if(elem_try.innerText.indexOf('please solve more.') >= 0){
-                    elem_try.parentNode.removeChild(elem_try);
-                    return true;
-                }
-            }
-            
-            var elem_anchor = parent.frames[0].document.getElementById
-                ("recaptcha-anchor");
-            if(elem_anchor.getAttribute("aria-checked") === "true"){
-                return true
-            }
+        func ="""(function() {
+    %s
 
-            
-        }"""% wants_true
+    checkbox_frame = parent.frames[0].document;
+    image_frame = parent.frames[1].document;
+
+    var bot_header = $(".rc-doscaptcha-header-text", image_frame)
+    if(bot_header.length){
+        if(bot_header.text().indexOf("Try again later") > -1){
+            parent.window.wasdetected = true;
+            return true;
+        }
+    }
+    
+    var try_again_header = $(".rc-audiochallenge-error-message", image_frame)
+    if(try_again_header.length){
+        if(try_again_header.text().indexOf("please solve more") > -1){
+            try_again_header.text('Trying again...')
+            return true;
+        }
+    }
+    
+    var checkbox_anchor = $("#recaptcha-anchor", checkbox_frame);
+    if(checkbox_anchor.attr("aria-checked") === "true"){
+        return true;
+    }
+    
+})()"""% wants_true
         try:
             await frame.waitForFunction(func, timeout=timeout * 1000)
         except:
