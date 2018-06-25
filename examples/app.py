e83e564dafd76b8fdd09276c30a42536cd9018d4
Michael Mooney <mikeyy@mikeyy.com>
2018-06-25 04:29:20 -0500

    making app.py more coroutine compatible

--- examples/app.py
+++ examples/app.py
@@ -3,20 +3,21 @@ import backoff
 import random
 import sys
 import time
-import threading
 
 from async_timeout import timeout
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from multiprocessing import cpu_count
+from threading import Thread
 from quart import Quart, Response, request
 
 from nonocaptcha import util
 from nonocaptcha.solver import Solver
 from config import settings
 
-# Max browsers to open/threads
-count = 10
-
+# Max threads to use in Pool
+threads = 100
 app = Quart(__name__)
-sem = asyncio.Semaphore(count)
 
 
 def shuffle(i):
@@ -24,51 +25,59 @@ def shuffle(i):
     return i
 
 
+def new_event_loop(pool_size=None):
+    pool_size = pool_size or cpu_count()
+    if sys.platform == 'win32':
+        loop = asyncio.ProactorEventLoop()
+        asyncio.set_event_loop(loop)
+    else:
+        loop = asyncio.new_event_loop()
+    thread_pool = ThreadPoolExecutor(pool_size)
+    loop.set_default_executor(thread_pool)
+    asyncio.set_event_loop(loop)
+    return loop
+
+
 proxies = None
 async def get_proxies():
     global proxies
+
+    asyncio.set_event_loop(new_event_loop())
     while 1:
         protos = ["http://", "https://"]
         if any(p in proxy_src for p in protos):
             f = util.get_page
         else:
             f = util.load_file
-    
-        result = await f(proxy_src)
-        proxies = iter(shuffle(result.strip().split("\n")))
-        await asyncio.sleep(10*60)
-
-
-def loop_proxies(loop):
-    asyncio.ensure_future(get_proxies(), loop=loop)
+        
+        try:
+            result = await f(proxy_src)
+        except:
+            continue
+        else:
+            proxies = iter(shuffle(result.strip().split("\n")))
+            await asyncio.sleep(10*60)
 
 
 async def work(pageurl, sitekey):
     # Chromium options and arguments
     options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
 
-    async with sem:
-        async with timeout(180) as t:
-            while 1:
-                if proxy_src:
-                    proxy = next(proxies)
-                else:
-                    proxy = None
-                
-                client = Solver(pageurl, sitekey, options=options, proxy=proxy)
-                answer = await client.start()
-                if answer:
-                    return answer
-                else:
-                    if t.expired:
-                        break
+    if proxy_src:
+        proxy = next(proxies)
+    else:
+        proxy = None
+    
+    client = Solver(pageurl, sitekey, options=options, proxy=proxy)
+    answer = await client.start()
+    if answer:
+        return answer
+
 
 @app.route("/get", methods=["GET", "POST"])
 async def get():
-    if proxy_src:
-        while proxies is None:
-            print('Proxies loading...')
-            await asyncio.sleep(1)
+    while not proxies:
+        await asyncio.sleep(1)
     
     if not request.args:
         result = "Invalid request"
@@ -78,23 +87,22 @@ async def get():
         if not pageurl or not sitekey:
             result = "Missing sitekey or pageurl"
         else:
-            result = await work(pageurl, sitekey)
+            async with timeout(180) as t:
+                while 1:
+                    task = asyncio.ensure_future(work(pageurl, sitekey), loop=loop)
+                    await task
+                    result = task.result()
+                    if result or t.expired:
+                        break
             if not result:
-                result = "Request timed-out, please try again"
+                result = "Request timed-out, please try again"           
     return Response(result, mimetype="text/plain")
 
 
-if __name__ == "__main__":
-
-    if sys.platform == 'win32':
-        loop = asyncio.ProactorEventLoop()
-        asyncio.set_event_loop(loop)
-    else:
-        loop = asyncio.get_event_loop()
+loop = new_event_loop()
+proxy_src = settings["proxy_source"]
+if proxy_src:
+    asyncio.ensure_future(get_proxies())
     
-    proxy_src = settings["proxy_source"]
-    if proxy_src:
-        t = threading.Thread(target=loop_proxies, args=(loop,))
-        t.start()
-
+if __name__ == "__main__":
     app.run("0.0.0.0", 5000, loop=loop)
