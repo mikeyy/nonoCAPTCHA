1f753dbc371ce009331fa2bea814cae6e41574ad
Michael Mooney <mikeyy@mikeyy.com>
2018-07-03 13:42:23 -0500

    Update description

--- nonocaptcha/__init__.py
+++ nonocaptcha/__init__.py
@@ -17,7 +17,7 @@ __author__ = ", ".join(
     "{} <{}>".format(name, email) for name, email in authors
 )
 
-package_info = "An asynchronized Python library to automate solving ReCAPTCHA v2 by audio, using Microsoft Azure's Speech-to-Text API. "
+package_info = "An asynchronized Python library to automate solving ReCAPTCHA v2 by audio from multiple services"
 
 __package_dir__ = os.path.dirname(os.path.abspath(__file__))
 
