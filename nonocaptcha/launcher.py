92ab5c510fcfe4ee3d5ba1f38601168a3c676a92
Michael Mooney <mikeyy@mikeyy.com>
2018-08-04 12:09:49 -0500

    Clean up for simplification

--- nonocaptcha/launcher.py
+++ nonocaptcha/launcher.py
@@ -6,7 +6,6 @@
 import asyncio
 import atexit
 import os
-import psutil
 import signal
 import sys
 import websockets
@@ -65,7 +64,7 @@ class Launcher(launcher.Launcher):
 
         def _close_process(*args, **kwargs):
             if not self.chromeClosed:
-                asyncio.ensure_future(self.killChrome())
+                asyncio.wait(asyncio.ensure_future(self.killChrome()))
 
         # dont forget to close browser process
         atexit.register(_close_process)
@@ -88,15 +87,8 @@ class Launcher(launcher.Launcher):
         if self.proc:
             if self.proc.returncode is None and not self.chromeClosed:
                 self.chromeClosed = True
-                if psutil.pid_exists(self.proc.pid):
-                    process = psutil.Process(self.proc.pid)
-                    for proc in process.children(recursive=True):
-                        try:
-                            proc.kill()
-                        except psutil._exceptions.NoSuchProcess:
-                            pass
-                    self.proc.kill()
-                    await self.proc.wait()
+                self.proc.terminate()
+                await self.proc.wait()
 
     async def killChrome(self):
         """Terminate chromium process."""
@@ -104,13 +96,7 @@ class Launcher(launcher.Launcher):
             try:
                 await self.connection.send("Browser.close")
                 await self.connection.dispose()
-            except asyncio.streams.IncompleteReadError:
-                pass
-            except websockets.exceptions.ConnectionClosed:
-                pass
-            except ConnectionResetError:
-                pass
-            except NetworkError:
+            except Exception:
                 pass
         if self._tmp_user_data_dir and os.path.exists(self._tmp_user_data_dir):
             # Force kill chrome only when using temporary userDataDir
