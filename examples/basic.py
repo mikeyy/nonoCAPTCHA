f5315642ab3e24630ab10229c4a3a362ab217980
Michael Mooney <mikeyy@mikeyy.com>
2018-08-04 12:21:21 -0500

    Basic usage example demonstrating cleanup handling

---
+++ examples/basic.py
@@ -0,0 +1,34 @@
+import asyncio
+import sys
+
+from nonocaptcha.solver import Solver
+
+if len(sys.argv) == 3:
+    pageurl, sitekey, proxy = sys.argv[1:]
+else:
+    print('Invalid amount of arguments (pageurl, sitekey, proxy)')
+    sys.exit(0)
+
+
+async def handle_signal(signame):
+    if not task.cancelled():
+        task.cancel()
+
+
+loop = asyncio.get_event_loop()
+for signame in ('SIGINT', 'SIGTERM'):
+    loop.add_signal_handler(
+                                getattr(signal, signame),
+                                lambda: asyncio.ensure_future(
+                                    handle_signal(signame)
+                                )
+                            )
+
+
+options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
+client = Solver(pageurl, sitekey, options=options, proxy=proxy)
+task = asyncio.ensure_future(client.start())
+result = loop.run_until_complete(task)
+loop.run_until_complete(client.cleanup())
+if result:
+    print(result)
