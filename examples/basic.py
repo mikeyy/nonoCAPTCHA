392086bb56a0a9ce47401891eb4fafdb15e015e6
Michael Mooney <mikeyy@mikeyy.com>
2018-08-06 13:20:17 -0500

    Drastic simplfying

--- examples/basic.py
+++ examples/basic.py
@@ -11,26 +11,12 @@ else:
     sys.exit(0)
 
 
-async def handle_signal(signame):
-    if not task.cancelled():
-        task.cancel()
-
-
-loop = asyncio.get_event_loop()
-for signame in ('SIGINT', 'SIGTERM'):
-    loop.add_signal_handler(
-                                getattr(signal, signame),
-                                lambda: asyncio.ensure_future(
-                                    handle_signal(signame)
-                                )
-                            )
-
-
 options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
 client = Solver(pageurl, sitekey, options=options, proxy=proxy)
-task = asyncio.ensure_future(client.start())
-result = loop.run_until_complete(task)
-loop.run_until_complete(client.cleanup())
-if not task.cancelled():
+try:
+    result = loop.run_until_complete(client.start())
+except CancelledError:
+    raise
+else:
     if result:
         print(result)
