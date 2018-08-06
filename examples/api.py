788e12b51e680267929e856e09950691dfc251aa
Michael Mooney <mikeyy@mikeyy.com>
2018-08-06 13:09:40 -0500

    Add multi-threaded support

--- examples/api.py
+++ examples/api.py
@@ -1,78 +1,101 @@
 import asyncio
+import logging
 import random
 import signal
 import shutil
 import sys
+import subprocess
 
-from aiohttp import web
-from asyncio import CancelledError
+from aiohttp import web, ClientSession, ClientError
+from async_timeout import timeout
+from asyncio import TimeoutError, CancelledError
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial, wraps
 from pathlib import Path
 
 from nonocaptcha import util
 from nonocaptcha.proxy import ProxyDB
 from nonocaptcha.solver import Solver
 
-SECRETKEY = "CHANGEME"
+SECRET_KEY = "CHANGEME"
 
-proxy_source = None  # Can be URL or file location
+proxy_source = None # Can be URL or file location
 proxies = ProxyDB(last_banned_timeout=45*60)
 
+pool = ThreadPoolExecutor()
+main_loop = asyncio.get_event_loop()
+asyncio.get_child_watcher().attach_loop(main_loop)
+app = web.Application()
+
+# Clear Chrome temporary profiles
 dir = f"{Path.home()}/.pyppeteer/.dev_profile"
 shutil.rmtree(dir, ignore_errors=True)
 
-app = web.Application()
-loop = asyncio.get_event_loop()
+
+def timer(duration):
+    def wrap(func):
+        @wraps(func)
+        async def wrap(*args, **kwargs):
+            async with timeout(duration):
+                return await func(*args, **kwargs)
+        return wrap
+    return wrap
 
 
-def shuffle(i):
-    random.shuffle(i)
-    return i
-
-
-async def work(pageurl, sitekey, proxy):
-    proxy = await proxies.get()
-    if proxy:
-        if "@" in proxy:
-            proxy_details = proxy.split("@")
-            proxy = proxy_details[1]
-            username, password = proxy_details[0].split(":")
-            proxy_auth = {"username": username, "password": password}
-        options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
-        client = Solver(
-            pageurl,
-            sitekey,
-            options=options,
-            proxy=proxy,
-            proxy_auth=proxy_auth
+@timer(duration=3*60)  # 180 seconds seems reasonable
+async def work(pageurl, sitekey, loop):
+    while 1:
+        fut = asyncio.run_coroutine_threadsafe(
+            proxies.get(), main_loop
         )
+        proxy = fut.result()
+        options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
+        client = Solver(pageurl, sitekey, options=options, proxy=proxy)
         try:
-            result = await client.start()
-            if isinstance(result, dict):
-                if 'code' in result:
-                    return result
+            task = loop.create_task(client.start())
+            await task
+            task.exception()
         except CancelledError:
-            return
+            break
+        else:
+            result = task.result()
+            if result:
+                if result['status'] == "detected":
+                    asyncio.run_coroutine_threadsafe(
+                        proxies.set_banned(proxy), main_loop
+                    )
+                else:
+                    if result['status'] == "success":
+                        return result['code']
 
+
+def sub_loop(pageurl, sitekey):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(
+        work(pageurl, sitekey, loop)
+    )
+    return result
+    
 
 async def get_solution(request):
-    params = await request.post()
+    params  = request.rel_url.query
     pageurl = params.get("pageurl")
     sitekey = params.get("sitekey")
     secret_key = params.get("secret_key")
-    response = {"error": ""}
     if not pageurl or not sitekey or not secret_key:
-        response["error"] = "invalid request"
+        response = {"error": "invalid request"}
     else:
-        if secret_key != SECRETKEY:
-            response["error"] = "unauthorized attempt"
+        if secret_key!= SECRET_KEY:
+            response = {"error": "unauthorized attempt logged"}
         else:
             if pageurl and sitekey:
-                result = await work(pageurl, sitekey)
+                f = partial(sub_loop, pageurl, sitekey)
+                result = await main_loop.run_in_executor(pool, f)
                 if result:
-                    if 'code' in result:
-                        response["solution"] = result['code']
-                    else:
-                        response["error"] = result['status']
+                    response = {"solution": result}
+                else:
+                    response = {"error": "worker timed-out"}
     return web.json_response(response)
 
 
@@ -87,7 +110,7 @@ async def load_proxies():
 
         try:
             result = await f(proxy_source)
-        except Exception:
+        except:
             continue
         else:
             proxies.add(result.split('\n'))
@@ -104,17 +127,20 @@ async def cleanup_background_tasks(app):
     await app["dispatch"]
 
 
+#  Not sure if I need these here, will check later loop.add_signal_handler
+#  might be the better option
 def signal_handler(signal, frame):
+    loop.stop()
+    loop.close()
     sys.exit(0)
-
 
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGHUP, signal_handler)
 
-app.router.add_get("/get", get_solution)
+app.router.add_get("/", get_solution)
 app.on_startup.append(start_background_tasks)
 app.on_cleanup.append(cleanup_background_tasks)
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=8000)
+    web.run_app(app, host="0.0.0.0", port=5000)
