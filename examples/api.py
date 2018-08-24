30426e7955cabb1a0e0477b9697f350f62ac330d
Michael Mooney <mikeyy@mikeyy.com>
2018-08-24 12:32:45 -0500

    Some chaotic improvements for cancelling tasks in context manager
    Along with performance enhancements, many hours were spent silencing warnings produced from destroyed or pending tasks. Despite my attempts, out of 100 spawns, one child throws an IncompleteReadError exception that appears impossible to catch. A relook is due for another day, but for now, I'm satisfied with the changes compared to the old code.

--- examples/api.py
+++ examples/api.py
@@ -21,9 +21,10 @@ SECRET_KEY = "CHANGEME"
 proxy_source = None  # Can be URL or file location
 proxies = ProxyDB(last_banned_timeout=45*60)
 
-pool = ThreadPoolExecutor()
-main_loop = asyncio.get_event_loop()
-asyncio.get_child_watcher().attach_loop(main_loop)
+loop = asyncio.get_event_loop()
+asyncio.set_child_watcher(asyncio.FastChildWatcher())
+asyncio.get_child_watcher().attach_loop(loop)
+
 app = web.Application()
 
 # Clear Chrome temporary profiles
@@ -32,66 +33,86 @@ shutil.rmtree(dir, ignore_errors=True)
 
 
 class TimedLoop(object):
-    cancelled = False
+    result = None
 
-    def __init__(self, coro, duration):
+    def __init__(self, coro, duration, executor=None):
         self.coro = coro
         self.duration = duration
+        self.pool = executor if executor else ThreadPoolExecutor()
 
-    def __enter__(self):
+    async def __aenter__(self):
+        self.parent_task = asyncio.Task.current_task()
+        self.parent_loop = asyncio.get_event_loop()
         return self
         
-    def __exit__(self, *args):
-        self.shutdown()
+    async def __aexit__(self, *args):
+        self.parent_loop.call_soon(
+            self.parent_loop.create_task, self.shutdown()
+        )
 
-    def start(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-        task = asyncio.ensure_future(self._seek_result())
-        task.add_done_callback(self._on_done)
-        trigger = partial(self.loop.create_task, self._cancel_task(task))
-        self.loop.call_later(self.duration, trigger)
-        self.loop.run_forever()
-        if not task.cancelled():
-            result = task.result()
-            return result
+    def __await__(self):
+        return (yield from self.start)
 
-    def shutdown(self, *args, **kwargs):
-        # Cancel all pending tasks in the loop
-        pending = [
-            self.loop.call_soon_threadsafe(partial(self._cancel_task, task))
-            for task in asyncio.Task.all_tasks()
-        ]
-        asyncio.wait(pending,loop=self.loop)
+    async def start(self):
+        on_complete = self.parent_loop.create_future()
+        self.pool.submit(self.setup_loop, on_complete)
+        result = await on_complete
+        return result
 
-    async def _seek_result(self):
+    def setup_loop(self, waiter):
+        thread_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(thread_loop)
+        task = thread_loop.create_task(self.continuous(thread_loop))
+        thread_loop.call_later(self.duration, task.cancel)
         try:
-            while 1:
-                task = asyncio.ensure_future(
-                    self.coro(),
-                    loop=self.loop
-                )
+            result = thread_loop.run_until_complete(task)
+        finally:
+            self.parent_loop.call_soon_threadsafe(
+                waiter.set_result, result
+            )
+            thread_loop.create_task(self.cancel(thread_loop))
+            try:
+                thread_loop.run_forever()
+            finally:
+                thead_loop.call_soon(thread_loop.close)
+
+    async def cancel(self, loop):       
+        def silence_gathered(future):
+            try:
+                future.result()
+            except Exception:
+                pass
+
+        tasks = [
+            task for task in asyncio.Task.all_tasks(loop=loop)
+            if asyncio.Task.current_task() is not task
+        ]
+        gathered = asyncio.gather(*tasks, loop=loop)
+        gathered.add_done_callback(silence_gathered)
+        gathered.cancel()
+        try:
+            await gathered
+        finally:
+            loop.call_soon(loop.stop)
+
+    async def continuous(self, loop):
+        try:
+            while True:
+                task = loop.create_task(self.coro())
                 await task
-                if not task.cancelled():
-                    result = task.result()
-                    if result:
-                        return result
+                result = task.result()
+                if result:
+                    return result
         except CancelledError:
-            await self._cancel_task(task)
-            raise CancelledError
-    
-    def _on_done(self, task):
-        self.loop.call_soon_threadsafe(self.loop.stop)
-        
-    async def _cancel_task(self, task):
-        if not task.done():
-            task.set_result(None)
-            await task
-    
+            loop.call_soon(task.cancel)
+
+    async def shutdown(self):
+        self.parent_task.cancel()
+
 
 async def work(pageurl, sitekey):
     proxy = asyncio.run_coroutine_threadsafe(
-        proxies.get(), main_loop
+        proxies.get(), loop
     ).result()
     options = {"ignoreHTTPSErrors": True, "args": ["--timeout 5"]}
     client = Solver(
@@ -104,12 +125,12 @@ async def work(pageurl, sitekey):
     if result:
         if result['status'] == "detected":
             asyncio.run_coroutine_threadsafe(
-                proxies.set_banned(proxy), main_loop
+                proxies.set_banned(proxy), loop
             )
         else:
             if result['status'] == "success":
                 return result['code']
-
+    
 
 async def get_solution(request):
     params = request.rel_url.query
@@ -124,8 +145,8 @@ async def get_solution(request):
         else:
             if pageurl and sitekey:
                 coro = partial(work, pageurl, sitekey)
-                with TimedLoop(coro, duration=10) as t:
-                    result = await main_loop.run_in_executor(pool, t.start)
+                async with TimedLoop(coro, duration=180) as t:
+                    result = await t.start()
                 print(result)
                 if result:
                     response = {"solution": result}
