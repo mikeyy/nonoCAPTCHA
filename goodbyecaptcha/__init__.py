685b081b4bc031c1fc5ed4a245fadea3668c5449
MacKey <mackeyfuturo@gmail.com>
2020-05-13 11:27:39 -0400

    Change to version 2.4.0
    No tested All :|

--- goodbyecaptcha/__init__.py
+++ goodbyecaptcha/__init__.py
@@ -4,7 +4,7 @@
 import os.path
 import sys
 
-version_info = (2, 3, 3)
+version_info = (2, 4, 0)
 __version__ = "{}.{}.{}".format(*version_info)
 
 authors = (("MacKey-255", "mackeyfuturo@gmail.com"), ("Michael Mooney", "mikeyy@mikeyy.com"))
